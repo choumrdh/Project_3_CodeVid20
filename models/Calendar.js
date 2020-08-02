e54c6c221859d219e00a0ea3e2357c13d4ffdd61
Michelle Chou <chou.m.rdh@gmail.com>
2020-08-02 15:13:30 -0700

    add monogodb to server.js

--- models/Calendar.js
+++ models/Calendar.js
@@ -1,4 +1,4 @@
-const { Schema, model } = reuqire("mongoose");
+const { Schema, model } = require("mongoose");
 
 const CalendarSchema = new Schema({
     _user: {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -8,17 +9,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-}
+  app.use(express.static("client/build"));
+};
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/calendar", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 // Define API routes here
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
