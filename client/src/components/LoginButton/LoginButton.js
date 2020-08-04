58c2740bff2d44e3eb1417c61feb5be28ee25700
tongtong ding <tding7@asu.edu>
2020-08-03 22:02:51 -0700

    material-ui

--- client/src/components/LoginButton/LoginButton.js
+++ client/src/components/LoginButton/LoginButton.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "react-bootstrap";
+import { Button } from "@material-ui/core";
 
 const LoginButton = () => {
     const { loginWithRedirect } = useAuth0();
 
     return (
-        <Button variant="primary" onClick={() => loginWithRedirect()}>Log In</Button>
+        <Button variant="contained" color="primary" onClick={() => loginWithRedirect()}>Log In</Button>
     );
 };
 
