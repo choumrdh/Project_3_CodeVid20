e61e1a78cca1b15a9b0c9a8d1572f864c7589c1f
NMosch <nmosch@gmail.com>
2020-08-05 21:24:25 -0700

    update layout of calendar page

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import history from "./utils/history";
 import { useAuth0 } from "@auth0/auth0-react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import NavBar from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
 import Loading from "./components/Loading/Loading";
 import Footer from "./components/Footer/Footer";
-// import CalendarPage from './pages/CalendarPage';
+import CalendarPage from './pages/CalendarPage';
 
 function App() {
   const { isLoading } = useAuth0();
@@ -24,8 +24,8 @@ function App() {
         <Route exact path="/" component={Home} />
       </Switch>
       <Footer />
-      {/* <CalendarPage/> */}
     </Router>
+    // <CalendarPage/>
 
   );
 }
--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { Grid } from '@material-ui/core';
-import Navbar from '../components/NavBar/NavBar'
-import Calendar from '../components/Calendar/Calendar'
+import Navbar from '../components/NavBar/NavBar';
+import Calendar from '../components/Calendar/Calendar';
 import SideBar from '../components/SideBar/SideBar';
+// import './style.css';
 
 export default function CalendarPage() {
     return (
-        <>
-            
-            <Grid container>
-            <Navbar />  
-                <Grid item xs={3}>
-                    <SideBar />
+        <>  
+            <Navbar />
+                <Grid container>
+                    <Grid item xs={3}>
+                        <SideBar />
+                    </Grid>
+                    <Grid item xs={9}>
+                        <Calendar />
+                    </Grid>
                 </Grid>
-                <Grid item xs={8}>
-                    <Calendar />
-                </Grid>
-            </Grid>
-             
         </>
     )
 }
