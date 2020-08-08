d2a8c4ed85cfc6aa79f51a8f8b259a135a1b040c
tongtong ding <tding7@asu.edu>
2020-08-08 14:59:30 -0700

    display username name in sidebar

--- client/src/pages/CalendarPage.js
+++ client/src/pages/CalendarPage.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Grid } from '@material-ui/core';
-// import Navbar from '../components/NavBar/NavBar'
 import Calendar from '../components/Calendar/Calendar'
 import SideBar from '../components/SideBar/SideBar';
-// import './style.css';
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 export default function CalendarPage() {
+    const { user } = useAuth0();
     return (
         <>
             <Grid container>
                 <Grid item xs={3}>
-                    <SideBar />
+                    <SideBar user={user} />
                 </Grid>
                 <Grid item xs={9}>
                     <Calendar />
