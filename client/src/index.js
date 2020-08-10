3234c5c25de48e7a574ad924204197a12cf2d019
Michelle Chou <chou.m.rdh@gmail.com>
2020-08-09 23:10:31 -0700

    modified index.js

--- client/src/index.js
+++ client/src/index.js
@@ -17,9 +17,9 @@ const onRedirectCallback = (appState) => {
 ReactDOM.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={process.env.REACT_APP_DOMAIN}
-      clientId={process.env.REACT_APP_CLIENT_ID}
-      audience={process.env.REACT_APP_AUDIENCE}
+      domain={process.env.AUTH0_DOMAIN}
+      clientId={process.env.AUTH0_CLIENT_ID}
+      // audience={process.env.REACT_APP_AUDIENCE}
       redirectUri={window.location.origin}
       onRedirectCallback={onRedirectCallback}>
       <App />
