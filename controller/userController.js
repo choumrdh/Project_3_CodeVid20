13a788295088644283d91a2f6594a4c6e7f4fa68
Michelle Chou <chou.m.rdh@gmail.com>
2020-08-12 09:03:52 -0700

    clean up code in models and add try catch to user controller

--- controller/userController.js
+++ controller/userController.js
@@ -2,28 +2,41 @@ const User = require("../models/User");
 
 module.exports = {
   create: async (req, res) => {
-    let found = await User.findOne({ userEmail: req.body.userEmail });
-    if (found) {
-      return res.json(found);
-    } else {
-      let newUser = new User(req.body);
-      let savedUser = await newUser.save();
-      return res.json(savedUser);
+    try {
+      let found = await User.findOne({ userEmail: req.body.userEmail });
+      if (found) {
+        return res.json(found);
+      } else {
+        let newUser = new User(req.body);
+        let savedUser = await newUser.save();
+        return res.json(savedUser);
+      }
+    } catch (error) {
+      res.status(500).json(error);
     }
   },
   find: async (req, res) => {
-    let found = await User.findOne({ userEmail: req.body.userEmail }); // is this find() correct?
-    res.json(found);
+    try {
+      let found = await User.findOne({ userEmail: req.body.userEmail });
+      res.json(found);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
   all: async (req, res) => {
-    let allUsers = await User.find({});
-    res.json(allUsers);
+    try {
+      let allUsers = await User.find({});
+      res.json(allUsers);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
   eventsByUser: async (req, res) => {
-    // const {_id} = req.params;
-    // console.log({_id})
-    // console.log(req.params.id)
-    const user = await User.findById(req.params.id).populate("events"); // populate('Event') is in reference to User.js ref "Event" in schema
-    res.json(user);
+    try {
+      let user = await User.findById(req.params.id).populate("events");
+      res.json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
   },
 };
