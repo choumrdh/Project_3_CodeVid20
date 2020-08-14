82ee55bc0cca51b66b709a25c9d6cd7c45de0cf7
NMosch <nmosch@gmail.com>
2020-08-13 21:11:08 -0700

    oh boy...

--- client/src/components/SideBar/SideBar.js
+++ client/src/components/SideBar/SideBar.js
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import { Card, CardActionArea, CardActions, CardContent, Typography, Divider, Button } from '@material-ui/core';
 import './style.css';
 import { format } from "date-fns";
 import API from "../../utils/API";
@@ -14,15 +8,15 @@ import API from "../../utils/API";
 const useStyles = makeStyles({
   root: {
     maxWidth: 500,
+    backgroundColor: "#8FB9A8"
   },
 });
 
 export default function SideBar({ user, submitTime }) {
   console.log("user", user)
   const classes = useStyles();
-
   const [upcomingEvents, setUpcomingEvents] = useState([]);
-
+  const [forceUpdate, setForceUpdate] = useState(false);
 
   const getUpcomingDates = (dayCount) => {
     const today = new Date().getTime();
@@ -38,42 +32,77 @@ export default function SideBar({ user, submitTime }) {
 
   useEffect(() => {
     console.log("update.....")
-    API.getUpcomingEventsByDates(getUpcomingDates(5),user.email)
+    API.getUpcomingEventsByDates(getUpcomingDates(5), user.email)
       .then(events => { setUpcomingEvents(events.data) })
-  },[submitTime])
+  }, [submitTime, forceUpdate])
 
-
+  const handleDelete = id =>{
+    console.log("id=", id)
+    API.deleteEvent(id)
+   .then(()=>{setForceUpdate(!forceUpdate)})
+   .catch(err=>console.log(err))
+}
 
   return (
-    <div id='sidebar'>
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Hello, {user.nickname} <img src={user.picture} className="profile" style={{ width: "30px" }} alt="profile"></img>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardContent>
+          <div style={{ color: "white", fontFamily: "Verdana, Geneva, Tahoma, sans-serif" }}>
+            <Typography 
+            gutterBottom 
+            variant="h5" 
+            component="h2">
+              Hello, {user.nickname} 
+              <img 
+              src={user.picture} 
+              className="profile" 
+              style={{ width: "30px" }} 
+              alt="profile"></img>
             </Typography>
-            {/* <Typography variant="body2" color="textSecondary" component="p">
-              <div> */}
-                {
-                  upcomingEvents.map((event, i)=>{
-                  return (<><Typography key={i} component="h5">{event.Date}: {event.startTime}</Typography>
-                          <Typography component="h6">{event.title}</Typography>
-                          <Typography gutterBottom variant="body1">{event.description}</Typography></>)
-                  })
-                }
-              {/* </div>
-          </Typography> */}
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          {/* {/* <Button size="small" color="primary">
-          Share
-        </Button> */}
-          {/* <Button size="small" color="primary">
-          Delete
-        </Button>  */}
-        </CardActions>
-      </Card>
-    </div >
+            <Typography 
+            style={{ fontWeight: "bold", color: "white" }} 
+            component="h3">
+              Schedule your events with CalenDate!
+              </Typography>
+            <Typography 
+            style={{ fontWeight: "bold", color: "white" }} 
+            component="h5">
+              Click the date you need to schedule an event and enter the details
+              </Typography>
+            <Divider />
+            {
+              upcomingEvents.map((event, i) => {
+                console.log("event=", event)
+                return (<>
+                  <Typography 
+                  key={i} 
+                  component="h5">
+                    {event.Date}: {event.startTime}
+                    <Button 
+                    size="small" 
+                    color="primary" 
+                    id={event._id} 
+                    onClick={()=>handleDelete(event._id)}>
+                      Delete
+                    </Button>
+                  </Typography>
+                  <Typography 
+                  style={{color: "white"}} 
+                  component="h6">{event.title}
+                  </Typography>
+                  <Typography 
+                  gutterBottom 
+                  style={{color: "white"}} 
+                  variant="body1">
+                    {event.description}
+                    </Typography>
+                    <Divider /></>)
+              })}
+          </div>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+      </CardActions>
+    </Card>
   );
 }
